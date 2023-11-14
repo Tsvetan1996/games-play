214b1c7d22394aaa12bc6ec4b5a43affd3dea15d
Tsvetan Atanasov <85298268+Tsvetan1996@users.noreply.github.com>
2023-11-14 16:57:29 +0200

    Add header component and styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,11 @@
+import Header from "./components/header/Header";
+
 function App() {
-  return <>Hello World!</>;
+  return (
+    <div id="box">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
